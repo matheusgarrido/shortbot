6c3375f942e2a222d50d4e6314110fe997b52396
Matheus Garrido <matheusgarrido10@hotmail.com>
2020-11-20 20:29:20 -0300

    Update region and text letter start's condition to commands

--- routes/readMessage.js
+++ routes/readMessage.js
@@ -1,33 +1,42 @@
-import guildModel from '../model/guildModel.js';
-import helpCommand from '../helper/commands/helpCommand.js';
+import * as guildService from "../service/guildService.js";
+import helpCommand from "../helper/commands/helpCommand.js";
 
 async function readMessage(client, event) {
-    console.log(event.guild.region);
-    const { content } = event;
-    const contentSplitted = content.split('..');
-    if (contentSplitted.length > 1 && !contentSplitted[0]) {
-        const message = content.substr(2).trim();
-        const command = message.split(' ')[0];
-        console.log(command);
-        switch(command){
-            case 'help':
-                //List all help functions
-                helpCommand(event);
-                break;
-            case 'list':
-                //List all shortcuts
-                break;
-            case 'create':
-                //Create a new shortcut
-                break;
-            case 'update':
-                //Update an exist shortcut or delete it
-                break;
-            default:
-                //Search a shortcut in DB
-                break;
-        }
+  const { content } = event;
+  const contentSplitted = content.trim().split("..");
+  /* Do:
+    If has content after the prefix
+    If is blank before the prefix
+    If content start with letter */
+  if (
+    contentSplitted.length > 1 &&
+    !contentSplitted[0] &&
+    contentSplitted[1].charAt(0).match(/[a-z]/i)
+  ) {
+    const message = content.substr(2).trim();
+    const command = message.split(" ")[0];
+    switch (command) {
+      case "help":
+        //List all help functions
+        helpCommand(event);
+        break;
+      case "list":
+        //List all shortcuts
+        break;
+      case "create":
+        //Create a new shortcut
+        break;
+      case "update":
+        //Update an exist shortcut or delete it
+        break;
+      default:
+        //Search a shortcut in DB
+        break;
     }
+    //Try update region if necessary
+    const { id, region } = event.guild;
+    guildService.setRegion(id, region);
+  }
 }
 
 export default readMessage;
--- service/guildService.js
+++ service/guildService.js
@@ -1,24 +1,33 @@
-import guildModel from '../model/guildModel.js';
+import guildModel from "../model/guildModel.js";
 
-async function getGuild(guildId){
-    const guild = await guildModel.findById({ _id: guildId });
-    return guild;
+async function getGuild(guildId) {
+  const guild = await guildModel.findById({ _id: guildId });
+  return guild;
 }
 
-async function setJoined(guildId, region){
-    await guildModel.create({ _id: guildId, region });
-    guildModel.save();
+async function setJoined(guildId, region) {
+  await guildModel.create({ _id: guildId, region });
+  guildModel.save();
 }
 
-async function setRejoined(guildId, region ){
-    await guildModel.findByIdAndUpdate({ _id: guildId }, { state: true, region });
-    guildModel.save();
+async function setRejoined(guildId, region) {
+  await guildModel.findByIdAndUpdate({ _id: guildId }, { state: true, region });
+  guildModel.save();
 }
 
-async function setEjected(guildId){
-    await guildModel.findByIdAndUpdate({ _id: guildId }, { state: false });
-    guildModel.save();
+async function setEjected(guildId) {
+  await guildModel.findByIdAndUpdate({ _id: guildId }, { state: false });
+  guildModel.save();
 }
 
+async function setRegion(guildId, region) {
+  const guild = await guildModel.findById(guildId);
+  if (guild.region !== region) {
+    await guild.update({ region });
+    guild.save();
+  } else {
+    console.log("A guilda já está atualizada.");
+  }
+}
 
-export { getGuild, setJoined, setRejoined, setEjected };
+export { getGuild, setJoined, setRejoined, setEjected, setRegion };
