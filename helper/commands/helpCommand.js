975fef6785fd5b661be7f8d047798931c58a3920
Matheus Garrido <matheusgarrido10@hotmail.com>
2020-11-20 20:55:37 -0300

    Initial command guide text

--- helper/commands/helpCommand.js
+++ helper/commands/helpCommand.js
@@ -1,6 +1,19 @@
-async function helpCommand(event){
-    const messageToSend = "Guia de ajuda";
-    event.channel.send(messageToSend);
+async function helpCommand(event) {
+  const helpTextsContent = {
+    title: "Guia de Comandos".toLocaleUpperCase(),
+    description: {
+      list: "Lista todos os atalhos criados pelos usuários do servidor.",
+      create: "Cria um novo atalho.",
+      update: "Gerenciamento de atalho (atualização ou exclusão).",
+    },
+  };
+  const { title, description } = helpTextsContent;
+  const messageToSend = `${title}
+:new: **'..create'** - ${description.create}
+:page_facing_up: **'..list'** -  ${description.list}
+:pencil: **'..update'** - ${description.update}`;
+
+  event.channel.send(messageToSend);
 }
 
 export default helpCommand;
