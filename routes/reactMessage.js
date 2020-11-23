724fa759d31a9a664e03e9842b2e819304bbabf3
Matheus Garrido <matheusgarrido10@hotmail.com>
2020-11-22 23:55:25 -0300

    Delete content with confirmation and change state to set name or value

--- routes/reactMessage.js
+++ routes/reactMessage.js
@@ -1,4 +1,5 @@
-import * as guildService from '../service/guildService.js';
+import * as guildService from "../service/guildService.js";
+import * as contentService from "../service/contentService.js";
 
 async function reactMessage(client, event, user) {
   /*Continue:
@@ -13,21 +14,90 @@ async function reactMessage(client, event, user) {
     const emoji = event._emoji.name;
     const idGuild = event.message.channel.guild.id;
     const guild = await guildService.getGuild(idGuild);
-    console.log(guild);
-    console.log(emoji);
 
-    switch (emoji) {
-      case '1️⃣':
-        console.log('Alterar nome');
-        break;
-      case '2️⃣':
-        console.log('Alterar valor');
-        break;
-      case '❌':
-        console.log('Excluir atalho');
-        break;
+    const { id: idContent, state } = guild.currentShortcut;
+    const content = await contentService.getContentById(idContent);
+
+    //If content exists
+    if (content) {
+      //Delete menu
+      if (state === "update_delete") {
+        switch (emoji) {
+          //Abort delete
+          case "❌":
+            await guildService.setCurrentShortCut(idGuild, false);
+            channel.send("Exclusão cancelada");
+            break;
+          //Confirm delete
+          case "✅":
+            await guildService.setCurrentShortCut(idGuild, false);
+            await contentService.deleteContent(idContent);
+            channel.send("Atalho excluído!");
+            break;
+          //Another reaction
+          default:
+            channel.send(
+              "Reação inválida.\nSelecione uma das opções listadas no menu."
+            );
+            break;
+        }
+      }
+      //If started update or is in name/value update
+      else if (
+        state === "update_name" ||
+        state === "update_value" ||
+        state === "'update_selectoption'"
+      ) {
+        switch (emoji) {
+          //Update name
+          case "1️⃣":
+            await guildService.setCurrentShortCut(
+              idGuild,
+              true,
+              idContent,
+              "update_name"
+            );
+            channel.send("Atualizar nome");
+            break;
+          //Update value
+          case "2️⃣":
+            await guildService.setCurrentShortCut(
+              idGuild,
+              true,
+              idContent,
+              "update_value"
+            );
+            channel.send("Alterar valor");
+            break;
+          //Delete shortcut
+          case "❌":
+            await guildService.setCurrentShortCut(
+              idGuild,
+              true,
+              idContent,
+              "update_delete"
+            );
+            const messageSent = await channel.send(
+              "✅ to delete\n❌ to cancel delete"
+            );
+            messageSent.react("✅");
+            messageSent.react("❌");
+            break;
+          //Another reaction
+          default:
+            channel.send(
+              "Reação inválida.\nSelecione uma das opções listadas no menu."
+            );
+            break;
+        }
+      }
     }
-    channel.send(user.username + ' reagiu! ');
+    //Not found in guild.currentShortcut
+    else {
+      channel.send(
+        "O conteúdo não está mais disponível para alteração no momento.\nCaso deseje alterá-lo, digite ..update seguido do nome."
+      );
+    }
   }
 }
 
--- routes/readMessage.js
+++ routes/readMessage.js
@@ -1,25 +1,24 @@
-import * as guildService from '../service/guildService.js';
-import * as messageService from '../service/messageService.js';
-import * as contentService from '../service/contentService.js';
-import helpCommand from '../helper/helpCommand.js';
-import listCommand from '../helper/listCommand.js';
-import { createCommand, setNewShortcutValue } from '../helper/createCommand.js';
-import { startUpdate, updateShortcutValue } from '../helper/updateCommand.js';
-import executeShortcut from '../helper/executeShortcut.js';
+import * as guildService from "../service/guildService.js";
+import * as messageService from "../service/messageService.js";
+import * as contentService from "../service/contentService.js";
+import helpCommand from "../helper/helpCommand.js";
+import listCommand from "../helper/listCommand.js";
+import { createCommand, setNewShortcutValue } from "../helper/createCommand.js";
+import { startUpdate, updateShortcutValue } from "../helper/updateCommand.js";
+import executeShortcut from "../helper/executeShortcut.js";
 
 async function readMessage(client, event) {
   const { content } = event;
-  const contentSplitted = content.trim().split('..');
+  const contentSplitted = content.trim().split("..");
 
   const { id, region } = event.guild;
 
   const guildData = await guildService.getGuild(id);
-  // console.log(guildData.currentShortcut);
   const state =
-    guildData.currentShortcut !== undefined
+    Object.keys(guildData.currentShortcut).length !== 0
       ? guildData.currentShortcut.state
-      : ' _ ';
-  const stateCommand = state.split('_');
+      : "_";
+  const stateCommand = state.split("_");
   /* Do:
     If has content after the prefix
     If is blank before the prefix
@@ -31,10 +30,10 @@ async function readMessage(client, event) {
       .trim()
       .charAt(0)
       .match(/^[a-z\u00E0-\u00FC]+$/i) ||
-      ['value'].includes(stateCommand[1]))
+      ["value"].includes(stateCommand[1]))
   ) {
     const message = content.substr(2).trim();
-    const command = message.split(' ')[0];
+    const command = message.split(" ")[0];
 
     //Get messages
     let language = await messageService.getMessagesByRegion(region);
@@ -46,25 +45,25 @@ async function readMessage(client, event) {
       cancelFalse,
     } = language.messages.crud;
     //List all help functions
-    if (command.toLowerCase() === 'help') {
+    if (command.toLowerCase() === "help") {
       helpCommand(event);
     }
     //If nobody in guild is creating or updating a shortcut
     else if (!(await guildService.isOnCreateOrUpdate(id)))
       switch (command.toLowerCase()) {
-        case 'list':
+        case "list":
           //List all shortcuts
           listCommand(event, message);
           break;
-        case 'create':
+        case "create":
           //Create a new shortcut
           createCommand(event, message);
           break;
-        case 'update':
+        case "update":
           startUpdate(event, message);
           //Update an exist shortcut or delete it
           break;
-        case 'cancel':
+        case "cancel":
           //Update an exist shortcut or delete it
           event.channel.send(cancelFalse);
           break;
@@ -75,27 +74,27 @@ async function readMessage(client, event) {
       }
     else {
       switch (command) {
-        case 'list':
-        case 'create':
-        case 'update':
+        case "list":
+        case "create":
+        case "update":
           //If is in update select option menu
-          if (['selectoption'].includes(stateCommand[1])) {
+          if (["selectoption"].includes(stateCommand[1])) {
             event.channel.send(selectOption);
           }
           //If is the shortcut value
-          else if (['value'].includes(stateCommand[1])) {
+          else if (["value"].includes(stateCommand[1])) {
             //Get value to finish creating or update an existing shortcut
-            stateCommand[0] === 'create'
+            stateCommand[0] === "create"
               ? setNewShortcutValue(event, message)
-              : console.log('EDIT');
+              : console.log("EDIT");
           }
           //If is the name
           else event.channel.send(reserved);
           break;
-        case 'cancel':
+        case "cancel":
           const guild = await guildService.setCurrentShortCut(id, false);
           //Delete shortcut draft if is canceling in creation
-          if (stateCommand[0] === 'create') {
+          if (stateCommand[0] === "create") {
             event.channel.send(cancelCreate);
             await contentService.deleteContent(guild.currentShortcut.id);
           }
@@ -105,9 +104,9 @@ async function readMessage(client, event) {
           }
           break;
         default:
-          stateCommand[0] === 'create'
+          stateCommand[0] === "create"
             ? setNewShortcutValue(event, message)
-            : console.log('EDIT');
+            : console.log("EDIT");
       }
     }
     //Try update region if necessary
